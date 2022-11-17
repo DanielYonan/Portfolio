64bc4aa669e677dd04661627da24b02c089c00f5
Daniel Yonan <daniel-y@live.se>
2022-11-17 13:59:32 +0100

    /

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function myFunction() {
     var x = document.getElementsByTagName("img")[0];
-    x.getAttributeNode("src").value = "/assets/personal-pic.png";
+    x.getAttributeNode("src").value = "assets/personal-pic.png";
   }  
   
 function rollover(myImage)
       {
   
-          myImage.src = "/assets/personal-pic-color.png";
+          myImage.src = "assets/personal-pic-color.png";
   
       } 
   
 function mouseaway(myImage)
       {
   
-          myImage.src = "/assets/personal-pic.png";
+          myImage.src = "assets/personal-pic.png";
   
       }  
       
