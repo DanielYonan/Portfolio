45aaeed3755220739714b496b01f977c2fb0dd3e
Daniel Yonan <daniel-y@live.se>
2022-11-17 13:36:24 +0100

    Portfolio Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    var x = document.getElementsByTagName("img")[0];
+    x.getAttributeNode("src").value = "/assets/personal-pic.png";
+  }  
+  
+function rollover(myImage)
+      {
+  
+          myImage.src = "/assets/personal-pic-color.png";
+  
+      } 
+  
+function mouseaway(myImage)
+      {
+  
+          myImage.src = "/assets/personal-pic.png";
+  
+      }  
+      
